151aba70c8513f2f6cf3e99c77e0b5dd07f489c7
Jim <mrjim.development@gmail.com>
2024-01-16 14:12:14 +0800

    Finished

--- app/page.js
+++ app/page.js
@@ -1,113 +1,161 @@
-import Image from 'next/image'
+"use client"
+import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
+import { ChatOpenAI } from '@langchain/openai';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+import { RunnablePassthrough, RunnableSequence } from "@langchain/core/runnables";
+
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase'
+import { OpenAIEmbeddings } from '@langchain/openai'
+import { createClient } from '@supabase/supabase-js'
+
+// import { retriever } from '../utils/retriever';
+import { combineDocuments } from '../utils/combineDocuments';
+import { formatConvHistory } from '../utils/formatConvHistory'
+
+
+import '../public/styles/index.css';
+
+import 'dotenv/config'
+
 
 export default function Home() {
+  const [userInput, setUserInput] = useState('');
+  const [convHistory, setConvHistory] = useState([]);
+
+  
+  const openAIApiKey = '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'
+
+  const llm = new ChatOpenAI({ 
+    openAIApiKey,
+    temperature: 0, //creativeness
+  });
+
+
+  const embeddings = new OpenAIEmbeddings({ openAIApiKey })
+  const sbApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqcmZ2c2hsdHN6a3VtYXlocm5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUzMzMyMTAsImV4cCI6MjAyMDkwOTIxMH0.guJ8Qs3TsL327Jfc0cCCrGo1tjJ_CrtdZJHm3vpLIKw'
+  const sbUrl = 'https://kjrfvshltszkumayhrns.supabase.co'
+  const client = createClient(sbUrl, sbApiKey)
+
+
+  const vectorStore = new SupabaseVectorStore(embeddings, {
+      client,
+      tableName: 'documents',
+      queryName: 'match_documents'
+  })
+
+  const retriever = vectorStore.asRetriever()
+
+  const standaloneQuestionTemplate = `Given some covnersation history (if any) a question, convert it to a standalone question. 
+  conversation history: {conv_history}
+  question: {question} 
+  standalone question:`
+
+  const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
+
+  const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided and the conversation history. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+  conversation history: {conv_history}
+  context: {context}
+  question: {question}
+  answer: `
+
+  const answerPrompt = PromptTemplate.fromTemplate(answerTemplate)
+
+  const standaloneQuestionChain = RunnableSequence.from([
+      standaloneQuestionPrompt,
+      llm,
+      new StringOutputParser()
+  ])
+
+  const retrieverChain = RunnableSequence.from([
+      prevResult => prevResult.standalone_question,
+      retriever,
+      combineDocuments
+  ])
+
+  const answerChain = RunnableSequence.from([
+      answerPrompt,
+      llm,
+      new StringOutputParser()
+  ])
+
+  const chain = RunnableSequence.from([
+      {
+          standalone_question: standaloneQuestionChain,
+          original_input: new RunnablePassthrough()
+      },
+      {
+          context: retrieverChain,
+          question: ({original_input}) => original_input.question,
+          conv_history: ({original_input}) => original_input.conv_history
+  
+      },
+      answerChain
+  ])
+  // const chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser()).pipe(retriever).pipe(combineDocuments).pipe(answerPrompt)
+
+
+  async function progressConversation() {
+      const chatbotConversation = document.getElementById('chatbot-conversation-container')
+      const question = userInput
+      setUserInput('');
+
+      // add human message
+      const newHumanSpeechBubble = document.createElement('div')
+      newHumanSpeechBubble.classList.add('speech', 'speech-human')
+      chatbotConversation.appendChild(newHumanSpeechBubble)
+      newHumanSpeechBubble.textContent = question
+      chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+
+      const response = await chain.invoke({
+          question: question,
+          conv_history: formatConvHistory(convHistory)
+      })
+
+      convHistory.push(question)
+      convHistory.push(response)
+
+      // add AI message
+      const newAiSpeechBubble = document.createElement('div')
+      newAiSpeechBubble.classList.add('speech', 'speech-ai')
+      chatbotConversation.appendChild(newAiSpeechBubble)
+      newAiSpeechBubble.textContent = response
+      chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    progressConversation(userInput);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <Head>
+        <title>Scrimba Chatbot</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&display=swap" rel="stylesheet" />
+      </Head>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <main>
+        <section className="chatbot-container">
+          <div className="chatbot-header">
+            {/* Use Image component for optimized image loading */}
+            {/* <Image src="/images/logo-scrimba.svg" className={styles.logo} alt="logo" /> */}
+            <p className="sub-heading">Knowledge Bank</p>
+          </div>
+          <div className="chatbot-conversation-container" id="chatbot-conversation-container">
+            {/* Chat content goes here */}
+          </div>
+          <form id="form" className="chatbot-input-container">
+            <input name="user-input" type="text" id="user-input" required value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+            <button id="submit-btn" className="submit-btn" onClick={handleSubmit}>
+                <img src="images/send.svg" className="send-btn-icon" />
+            </button>
+          </form>
+        </section>
+      </main>
+    </div>
   )
 }
